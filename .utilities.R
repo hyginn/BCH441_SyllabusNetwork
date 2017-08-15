9016647a94edd75feff3ee819a410e5c750dcf91
hyginn <boris.steipe@utoronto.ca>
2017-08-15 07:05:45 +0200

    sources include.R and src2wtxt.R

--- .utilities.R
+++ .utilities.R
@@ -3,7 +3,7 @@
 # Miscellaneous R code to suppport the project
 #
 # Version: 1.0
-# Date:    2016 12
+# Date:    2017 08 15
 # Author:  Boris Steipe
 #
 # V 1.0    First code
@@ -13,32 +13,12 @@
 #
 # ==============================================================================
 
-objectInfo <- function(x) {
-    # Function to combine various information items about R objects
-    #
-    # Input: an R object
-    # Value: none - prints information as side-effect
+source("include.R")
+source("src2wtxt.R")
 
-    cat("object contents:")
-    print(x, digits = 22)  # print value at maximal precision
-
-    cat("\nstructure of object:\n")
-    str(x)
-
-    if (! is.list(x)) { # Don't use cat() if x is a list. cat() can't handle lists.
-        cat("\nmode:   ", mode(x), "\n")
-        cat("typeof: ", typeof(x), "\n")
-        cat("class:  ", class(x), "\n")
-    }
-
-    # if the object has attributes, print them too
-    if (! is.null(attributes(x))) {
-        cat("\nattributes:\n")
-        attributes(x)
-    }
-    # Done
+vspace <- function() {
+  return("\n{{Vspace}}\n\n")
 }
-
 
 
 # [END]
